e0f5c5375e9eca0c5b421ef6292ef247d14d4b26
Kyle Domingo <kdomingo@wayfair.com>
2023-03-19 18:51:35 +0100

    spring logging works

--- backend/src/main/java/com/kedomingo/blog/backend/controller/HelloController.java
+++ backend/src/main/java/com/kedomingo/blog/backend/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.kedomingo.blog.backend.controller;
 import io.micrometer.core.annotation.Timed;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class HelloController {
 
+  @SneakyThrows
   @GetMapping(path = "hello", produces = MediaType.APPLICATION_JSON_VALUE)
   @Operation(summary = "Hello World")
   @Timed(value = "method_call",
@@ -24,6 +26,13 @@ public class HelloController {
   public ResponseEntity<String> hello() {
 
     log.info("Hello World from HelloController");
+    if (1 > 0) {
+      try {
+        throw new Exception("This is a test exception");
+      } catch (Exception e) {
+        log.error(e.getMessage(), e);
+      }
+    }
     return new ResponseEntity<>("Hello World", HttpStatus.OK);
   }
 }
